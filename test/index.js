257fd568bb6e638732660370bdd5e161f2d1094f
Kenneth Lee <kenneth.lee@cbc.ca>
2014-09-09 20:28:53 -0400

    initial test

---
+++ test/index.js
@@ -0,0 +1,13 @@
+var app = require('../index'),
+	ioc = require('socket.io-client');
+
+describe('Server', function() {
+	it('Login should spawn a player', function(done) {
+		//var client = ioc('ws://0.0.0.0:54017');
+
+		//client.on('connect', function() {
+			done();
+		//});
+
+	});
+});
